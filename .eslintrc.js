677bc895ca18450b722e717483e6838183fcc3aa
Your Name <you@example.com>
2023-04-25 20:15:43 +0300

    feat: add eslint config. add static keyboard layout

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2, { 'SwitchCase': 1 }
+    ],
+    'linebreak-style': [
+      'error',
+      'windows'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-unused-vars': [
+      'warn'
+    ]
+
+  }
+};
